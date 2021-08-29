a2d866b3b9d9e6bbedd3e0d01c2023c9c195d9e9
Stanislav Silin <silin.stanislav@outlook.com>
2021-08-29 20:52:08 +0300

    Extend benchmark.

--- src/AttributesExtractor.Benchmark/Program.cs
+++ src/AttributesExtractor.Benchmark/Program.cs
@@ -11,6 +11,9 @@ namespace AttributesExtractor.Benchmark
     {
         public static void Main()
         {
+            // new AttributeBenchmark().Reflection();
+            // new AttributeBenchmark().AttributeExtractor();
+            
             BenchmarkRunner.Run<AttributeBenchmark>();
         }
     }
@@ -22,28 +25,51 @@ namespace AttributesExtractor.Benchmark
 
         public AttributeBenchmark()
         {
-            _user = new User();
+            _user = new User
+            {
+                FirstName = "Test"
+            };
+        }
+
+        [Benchmark]
+        public string Direct()
+        {
+            return _user.FirstName;
         }
         
         [Benchmark]
-        public bool Reflection()
+        public string Reflection()
         {
             var type = _user.GetType();
             var property = type.GetProperty(nameof(User.FirstName));
 
-            var result = property.GetCustomAttributes().Any(o => o.GetType() == typeof(RequiredAttribute));
-            
-            return result;
+            var required = property.GetCustomAttributes().Any(o => o.GetType() == typeof(RequiredAttribute));
+            if (required)
+            {
+                return (string)property.GetMethod?.Invoke(_user, null);
+            }
+
+            return string.Empty;
         }
-        
+
         [Benchmark]
-        public bool AttributeExtractor()
+        public string AttributeExtractor()
         {
             var entries = _user.GetProperties();
             var firstName = entries.First(o => o.Name == nameof(User.FirstName));
 
-            var result = firstName.Attributes.Any(o => o.Type == typeof(RequiredAttribute));
-            return result;
+            var required = firstName.Attributes.Any(o => o.Type == typeof(RequiredAttribute));
+            if (required)
+            {
+                if (firstName.TryGetValue(_user, out var value))
+                {
+                    return (string)value;
+                }
+
+                return string.Empty;
+            }
+
+            return string.Empty;
         }
     }
 }
